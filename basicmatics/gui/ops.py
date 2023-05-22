73f3d6cf758a7082f5b1582318a3942ff82e791d
MagicStar7213 <87906759+MagicStar7213@users.noreply.github.com>
2023-05-22 17:06:11 +0200

    Added aproximation functionality

--- basicmatics/gui/ops.py
+++ basicmatics/gui/ops.py
@@ -1,18 +1,30 @@
-def is_int(a: float):
-    if a.is_integer():
-        return int(a)
+from basicmatics.data.settings import *
+
+aprox = get_aprox()
+
+
+def is_int(number: float):
+    if number.is_integer():
+        return int(number)
     else:
-        return a
+        return number
 
 
-def calc(a: float, b: float, clc: str):
+def is_aprox(number: float):
+    if aprox == 1:
+        return round(number, 3)
+    else:
+        return number
+
+
+def calc(number1: float, number2: float, clc: str):
     if clc == 'dvd':
-        return is_int(a/b)
+        return is_aprox(is_int(number1/number2))
     elif clc == 'dvdr':
-        return is_int(a%b)
+        return is_aprox(is_int(number1%number2))
     elif clc == 'mtp':
-        return is_int(a*b)
+        return is_aprox(is_int(number1*number2))
     elif clc == 'sub':
-        return is_int(a-b)
+        return is_aprox(is_int(number1-number2))
     elif clc == 'add':
-        return is_int(a+b)
+        return is_aprox(is_int(number1+number2))
